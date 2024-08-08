b8efa7837e09f206107709a1653204c1a70be173
Akshara112 <akshara112000@gmail.com>
2024-08-09 02:34:43 +0530

    excel file added, assignment modified

--- day8/assignment.py
+++ day8/assignment.py
@@ -1,28 +1,3 @@
-'''i = "MATHS, ART"
-print(type(i))
-listMarks = i.split(',')
-print(type(listMarks))
-print(len(listMarks))'''
-
-# try:
-#input branch: 1   #only 1
-#input pref : 1,2,3  #1 or more
-#input marks: 97,96,97   #exact 3
-# except:
-    #please provide valid inputs
-
-#use dictionary to convert console number input to actual text
-
-# occupation=='3'
-
-# branch loop
-    # pref loop
-        #applicant should be pass in all subject
-            # if #Marketing (ECE): Art>90 + Literature>90, pass in all subject (maths > 35)
-            # elif Accounts (BCOM): Maths>95, pass in all subject (English and Arts > 35)
-            # elif Sales (MECH): Maths>90 + Literature>90, pass in all subject (Art > 35)
-            # else : no openings for provided creiteria
-
 
 branch_dict = {
     1:'ECE',
@@ -30,20 +5,26 @@ branch_dict = {
     3:'MECH'
 }
 try:
+    #input for branch
     branch_number = int(input("Enter the branch (1: ECE, 2: BCOM, 3: MECH): "))
     branch_name = branch_dict[branch_number]
 
+    #input for preferences
     preferences_input = input("Input preferences separated by ',' (1: Maths, 2: English, 3: Arts) : ")
+    #convert input string to a list of integers
     preferences = [int(preference) for preference in preferences_input.split(',')]
 
+    #input for marks
     marks_input = input("Enter marks for Maths, English, Arts separated by ',' : ")
+    #convert input string to a list of integers
     marks = [int(mark) for mark in marks_input.split(',')]
-    maths, literature, art  = marks
+    #assign marks to 3 different variables
+    maths, literature, art  = marks                  
 
 except:
     print("Enter a valid input")
 
-
+#check pass condition all marks are above pass mark35
 pass_mark = (art > 35 and literature > 35 and maths > 35)
 if pass_mark:
     for pref in preferences:
